334c706cdf8dab078285fe20e81dab23e9b11be5
eliseo.nava <eliseo.nava@cbtis128.edu.mx>
2026-03-23 23:19:57 +0000

    mas otro 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Login App',
+      title: 'Login flutter',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         useMaterial3: true,
